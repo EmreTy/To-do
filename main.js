aab94e4f94b3d8be6883b63b5525d44242fc4044
EmreTy <Emret@t-online.de>
2021-10-19 15:56:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+console.log("Hello To Do App!");
+
+function addNewTodo() {
+  const newTodoEl = document.querySelector("#new-todo");
+  const todoValue = newTodoEl.value.trim();
+
+  if (todoValue.length === 0) {
+    return;
+  }
+
+  if (isDuplicate(todoValue)) {
+    alert("Try one more time, already exist!");
+    return;
+  }
+
+  const newLi = document.createElement("li");
+  newLi.innerText = todoValue;
+  //console.log(todoValue);
+
+  newLi.setAttribute("data-todo", todoValue.toLowerCase());
+
+  const listEl = document.querySelector("#todo-list");
+  listEl.appendChild(newLi);
+
+  //put in a new checkbox
+
+  const newBox = document.createElement("input");
+  newBox.setAttribute("type", "checkbox");
+  newLi.appendChild(newBox);
+
+  newTodoEl.value = "";
+}
+
+const addTodoBtn = document.querySelector("#add-todo");
+addTodoBtn.addEventListener("click", addNewTodo);
+
+function isDuplicate(todo) {
+  todo = todo.toLowerCase();
+  const todoListEl = document.querySelector("#todo-list");
+
+  for (let i = 0; i < todoListEl.children.length; i++) {
+    const currentLiEl = todoListEl.children[i];
+    const currentTodo = currentLiEl.getAttribute("data-todo");
+    if (currentTodo === todo) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+//Da wird überstreicheln erledigt
+const itemChecked = document.querySelector("#todo-list");
+
+itemChecked.addEventListener("change", isChecked);
+
+function isChecked(event) {
+  const checkbox = event.target;
+  if (checkbox.checked === true) {
+    checkbox.parentElement.classList.add("done");
+  } else {
+    checkbox.parentElement.classList.remove("done");
+  }
+}
